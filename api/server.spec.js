c41c1d97fc9fa999235a3d9055309d78f8441f36
MISC USER <MISC@Sabrinas-MacBook-Pro.local>
2019-06-28 11:47:16 -0700

    added some fixes

--- api/server.spec.js
+++ api/server.spec.js
@@ -20,35 +20,35 @@ describe('server.js', () => {
                     expect(res.body.message).toEqual('Successful')
                 ])
         })
+        it('return an array if no games', async() => {
+            await supertest(server)
+                .get('/games')
+                .then(res => {
+                    // expect(res.body.games).toEqual([])
+                })
+        })
     })
     describe('POST /games', () => {
         const game = {
                 title: 'mortal kombat',
                 genre: 'fighting'
         }
-
-        it('return with 201 status', async() => {
+        it('missing game data', async() => {
             await supertest(server)
                 .post('/games')
-                .send(game)
+                // .send(game)
+                .expect(422)
+        })
+        it('return with 201 status', () => {
+            return supertest(server)
+                .post('/games')
+                // .send(game)
                 .expect(201)
         })
         it('return a JSON object', async() =>{
             await supertest(server)
                 .post('/games')
                 .expect('Content-Type', /json/i)
-        })
-        it('missing game data', async() => {
-            await supertest(server)
-                .post('/games')
-                .send(game)
-                .then(res => {
-                    if(res.body.title && res.body.genre) {
-                        return expect(201)
-                    } else {
-                        return expect(422)
-                    }
-                })
         })
     })
 })
