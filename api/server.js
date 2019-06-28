30f7b8f7edd8cea6a468c4b97c9277d177608994
MISC USER <MISC@Sabrinas-MacBook-Pro.local>
2019-06-28 11:05:00 -0700

    adding tests for endpoints

--- api/server.js
+++ api/server.js
@@ -2,7 +2,46 @@ const express = require('express')
 
 const server = express()
 
+const Games = require('./games-model')
+
 server.use(express.json())
+
+server.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome to the server'
+    })  
+})
+
+server.get('/games', async(req, res) => {
+    try {
+        const games = await Games.get()
+        res.status(200).json({
+            games,
+            message: 'Successful'
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error retrieving games'
+        })
+    }
+})
+
+server.post('/games', async(req, res) => {
+    try {
+        const game = await Games.add(req.body)
+        if(game.title && game.genre) {
+            res.status(201).json(game)            
+        } else {
+            res.status(422).json({
+                message:'Please provide title and genre of the game'
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error adding game'
+        })
+    }
+})
 
 
 
--- api/server.spec.js
+++ api/server.spec.js
@@ -1,8 +1,54 @@
-const request = require('supertest')
+const supertest = require('supertest')
 const server = require('./server')
 
 describe('server.js', () => {
-    describe('index route', () => {
-        
+    describe('GET /games', () => {
+        it('return with 200 status', async () => {
+            await supertest(server)
+                .get('/games')
+                .expect(200)
+        })
+        it('return a JSON object', async () => {
+            await supertest(server)
+                .get('/games')
+                .expect('Content-Type', /json/i)
+        })
+        it('return message successful', async() => {
+            await supertest(server)
+                .get('/games')
+                .then(res => [
+                    expect(res.body.message).toEqual('Successful')
+                ])
+        })
+    })
+    describe('POST /games', () => {
+        const game = {
+                title: 'mortal kombat',
+                genre: 'fighting'
+        }
+
+        it('return with 201 status', async() => {
+            await supertest(server)
+                .post('/games')
+                .send(game)
+                .expect(201)
+        })
+        it('return a JSON object', async() =>{
+            await supertest(server)
+                .post('/games')
+                .expect('Content-Type', /json/i)
+        })
+        it('missing game data', async() => {
+            await supertest(server)
+                .post('/games')
+                .send(game)
+                .then(res => {
+                    if(res.body.title && res.body.genre) {
+                        return expect(201)
+                    } else {
+                        return expect(422)
+                    }
+                })
+        })
     })
 })
